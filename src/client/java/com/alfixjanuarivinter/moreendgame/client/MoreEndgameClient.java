9114f2b96ef97a76763d13085d75f57e20352b64
Alfix <alfreddahlin0@gmail.com>
2026-06-30 20:20:37 +0200

    Remote repo first commit

---
+++ src/client/java/com/alfixjanuarivinter/moreendgame/client/MoreEndgameClient.java
@@ -0,0 +1,10 @@
+package com.alfixjanuarivinter.moreendgame.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreEndgameClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/alfixjanuarivinter/moreendgame/client/MoreEndgameDataGenerator.java
@@ -0,0 +1,11 @@
+package com.alfixjanuarivinter.moreendgame.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreEndgameDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
